7c0bc0a650db9c4acb6d1e7ed02a5bfa4fb03747
Elmeri Savioja <62933497+adome22@users.noreply.github.com>
2025-12-05 15:25:54 +0200

    update cors routes

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,9 @@ app.use(cors({
     'http://localhost:5000',
     'http://localhost:3000',
     'http://localhost:5500',
-    'http://127.0.0.1'
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:5000',
+    'http://127.0.0.1:5500'
   ],
 }));
 app.use(express.json());
